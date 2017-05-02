d359f2d3f6cb73bb5708f807dc8cfb141e14bc4e
Chris Gerke <chris.gerke@gmail.com>
2017-05-02 10:31:15 +0800

    Lint

--- tests/run_linters.sh
+++ tests/run_linters.sh
@@ -4,3 +4,8 @@ shellcheck root/private/var/root/bootstrap.sh
 shellcheck root/private/var/root/macos.sh
 shellcheck -x scripts/postinstall
 shellcheck -x scripts/preinstall
+#bashate root/etc/rc.installer_cleanup
+#bashate root/private/var/root/bootstrap.sh
+#bashate root/private/var/root/macos.sh
+#bashate -x scripts/postinstall
+#bashate -x scripts/preinstall 
