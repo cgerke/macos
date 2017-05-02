62c408c95d1737f53cab8497da742caa0e884afe
Chris Gerke <chris.gerke@gmail.com>
2017-05-02 10:57:39 +0800

    Travis

--- tests/run_linters.sh
+++ tests/run_linters.sh
@@ -2,10 +2,10 @@
 shellcheck root/etc/rc.installer_cleanup
 shellcheck root/private/var/root/bootstrap.sh
 shellcheck root/private/var/root/macos.sh
-shellcheck -x scripts/postinstall
-shellcheck -x scripts/preinstall
+shellcheck scripts/postinstall
+shellcheck scripts/preinstall
 #bashate root/etc/rc.installer_cleanup
 #bashate root/private/var/root/bootstrap.sh
 #bashate root/private/var/root/macos.sh
 #bashate -x scripts/postinstall
-#bashate -x scripts/preinstall 
+#bashate -x scripts/preinstall
